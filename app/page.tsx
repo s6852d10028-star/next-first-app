1bae145d97f3da20be4e9d6a75276fe6ad68eb7a
Rujirada <s6852d10028@sau.ac.th>
2026-06-21 11:38:44 +0700

    commit : name

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,10 @@ export default function Page() {
   return (
     <div>
       Hello....Main Page
+      <br />
+      ID: 6852D10028
+      <br />
+      Name: Rujirada Hongsingthong
     </div>
   );
 }
